15b7ce70d701da7878f1e053441e71ba4985b964
raphhax <raphaelhenriquedev@gmail.com>
2025-11-15 17:31:19 -0300

    Organizando arquivo principal

--- script.js
+++ script.js
@@ -1,48 +1,44 @@
 const { chromium } = require('playwright');
 
-async function teste() {
+async function automatizacaoCompra() {
 
     // Inicia o navegador (headless: false para vê-lo abrindo)
     const browser = await chromium.launch({ headless: false, slowMo:500 });
-    const context = await browser.newContext(); //anon
+    
+    // Abre via guia anonima por risco de problemas com cookies de login
+    const context = await browser.newContext();
 
-    // Abre uma nova aba/página
     const page = await context.newPage();
+    // Site exclusivo para treinamento de web scrapings
+    await page.goto('https://www.scrapingcourse.com/'); 
 
-    // Navega até a URL
-    await page.goto('https://www.scrapingcourse.com/');
-
+    // Processo para realizacao de login na plataforma
     await page.locator('a[href="https://www.scrapingcourse.com/login"]').click();
     await page.fill("#email", "admin@example.com");
-     await page.fill("#password", "password");
+    await page.fill("#password", "password");
     await page.locator("button[type='submit']").click();
+
+    // Retornando a aba inicial e procurando pelo local desejado do site para o web scraping => Ecommerce
     await page.locator("#logo-link").click();
     await page.locator('a[href="/ecommerce"]').click();
+
+    // Busca por determinado produto
     await page.fill("#wp-block-search__input-1", "Beaumont Summit Kit");
     await page.locator('button.wp-block-search__button.wp-element-button').click();
     await page.locator('a[href="https://www.scrapingcourse.com/ecommerce/product/beaumont-summit-kit/"]').click();
+
+    // Especificacoes do produto na finalizacao do pedido
     await page.locator('#size').selectOption('M');
     await page.locator('#color').selectOption('Orange');  
     await page.locator('input[name="quantity"]').fill('2'); 
+
+    // Adicao do produto ao carrinho e conferencia final
     await page.locator('.single_add_to_cart_button').click();
     await page.locator('.cart-contents').click();
-    
 
-
-
-    //let pagina=await page.locator('a[href="https://www.scrapingcourse.com/pagination/13"]').textContent()
-    //console.log(pagina);
-
-    /*
-    for(let i = 0; i<pagina;){
-
-    }
-    */
-    // Imprime o título da página no console
-    //console.log(await page.title());
 
     // Fecha o navegador
     //await browser.close();
 }
 
-teste();
+automatizacaoCompra();
